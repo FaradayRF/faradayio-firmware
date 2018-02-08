59ae83591c706fd09b395c916ff824cffe90cafd
Brent <brent@faradayrf.com>
2018-02-07 22:37:34 -0800

    Adding initializations folder with UCS files.

---
+++ initializations/init_ucs.c
@@ -0,0 +1,10 @@
+/*
+ * init_ucs.c
+ *
+ *  Created on: Feb 7, 2018
+ *      Author: KB1LQ
+ */
+
+
+
+
---
+++ initializations/init_ucs.h
@@ -0,0 +1,15 @@
+/*
+ * init_ucs.h
+ *
+ *  Created on: Feb 7, 2018
+ *      Author: KB1LQ
+ */
+
+#ifndef INITIALIZATIONS_INIT_UCS_H_
+#define INITIALIZATIONS_INIT_UCS_H_
+
+
+
+
+
+#endif /* INITIALIZATIONS_INIT_UCS_H_ */
