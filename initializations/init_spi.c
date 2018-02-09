bae9d76581ecc7e6243df4bc82d691090cc5ffc6
Brent <brent@faradayrf.com>
2018-02-09 08:52:43 -0800

    Add SPI init file in basic first variable introduction.

---
+++ initializations/init_spi.c
@@ -0,0 +1,19 @@
+/*
+ * init_spi.c
+ *
+ *  Created on: Feb 9, 2018
+ *      Author: KB1LQ
+ */
+
+#include "driverlib.h"
+#include "init_spi.h"
+
+/**
+ * Specify desired frequency of SPI communication
+ */
+#define SPICLK                          500000
+
+uint8_t transmitData = 0x00;
+uint8_t receiveData = 0x00;
+uint8_t returnValue = 0x00;
+
---
+++ initializations/init_spi.h
@@ -0,0 +1,28 @@
+/*
+ * init_spi.h
+ *
+ *  Created on: Feb 9, 2018
+ *      Author: KB1LQ
+ */
+
+#ifndef INITIALIZATIONS_INIT_SPI_H_
+#define INITIALIZATIONS_INIT_SPI_H_
+
+/**
+ * Transmit data byte value
+ */
+extern uint8_t transmitData;
+
+/**
+ * Receive data byte value
+ */
+extern uint8_t receiveData;
+
+/**
+ * Return byte data byte value
+ */
+extern uint8_t returnValue;
+
+
+
+#endif /* INITIALIZATIONS_INIT_SPI_H_ */
