4196207e108ccaef3187bdbaf204dfaed2cb56ef
Brent <brent@faradayrf.com>
2018-02-08 23:27:31 -0800

    Adding initial Doxygen documentation support.

---
+++ Documentation/html/search/searchdata.js
@@ -0,0 +1,21 @@
+var indexSectionsWithContent =
+{
+  0: "abcdefglmoprstuw",
+  1: "abcdefglmoprstuw",
+  2: "f"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "groups",
+  2: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Modules",
+  2: "Pages"
+};
+
