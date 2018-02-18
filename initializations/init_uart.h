66154ce5950fd2cff8f0daa7fffe7eac9f14939e
Brent <brent@faradayrf.com>
2018-02-17 16:02:24 -0800

    Created initial uart init files.

---
+++ initializations/init_uart.h
@@ -0,0 +1,16 @@
+/*
+ * init_uart.h
+ *
+ *  Created on: Feb 17, 2018
+ *      Author: KB1LQ
+ */
+
+#ifndef INITIALIZATIONS_INIT_UART_H_
+#define INITIALIZATIONS_INIT_UART_H_
+
+extern uartreceivedData;
+extern uarttransmitData;
+extern check;
+
+
+#endif /* INITIALIZATIONS_INIT_UART_H_ */
